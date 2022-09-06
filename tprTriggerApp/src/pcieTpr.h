62b92b7dbc368d218ce827a48e8375252687e159
Kukhee Kim <khkim@slac.stanford.edu>
2022-09-05 21:06:30 -0700

    support generalTime and soft event

--- tprTriggerApp/src/pcieTpr.h
+++ tprTriggerApp/src/pcieTpr.h
@@ -4,11 +4,21 @@
 #include <stdint.h>
 #include "tprsh.hh"
 
+#include "timingFifoApi.h"
 
 extern "C" {
 
+void   pcieTprSetSoftEv(int idx, int ev_num, bool ev_enable);
+void   pcieTprSetChEv(int ev_num, bool ev_enable);
+void   pcieTprInitSoftEv(void);
+int    pcieTpr_evPrefix(char *dev_prefix);
 void * pcieTprInit(char *dev_prefix);
 void * pcieTprGPWrapper(void);
+
+TimingPulseId timingGetLastFiducial();
+int timingGetCurrTimeStamp(epicsTimeStamp *ptine);
+int timingGetEventTimeStamp(epicsTimeStamp *ptime, int eventCode);
+
 
 }
 
