5e1a2bf88eead44d458595c09613b9134da95b5d
Michael Browne <mcbrowne@slac.stanford.edu>
2023-07-13 09:04:27 -0700

    Update to support timingApi R0.10

--- tprTriggerApp/src/pcieTpr.cpp
+++ tprTriggerApp/src/pcieTpr.cpp
@@ -31,6 +31,9 @@
 #include "pcieTpr.h"
 #include "tprTriggerAsynDriver.h"
 
+extern "C" {
+    int lcls1_timing = 0;
+};
 
 #if 0
 static uint64_t __rdtsc(void){
@@ -234,13 +237,13 @@ static void tprChannelFunc(void *param)
         prev_allrp = allrp;
 
 	volatile time_st_t *ts    = (volatile time_st_t *) dp;
-	int lcls1_mode = ts->mode||((ts->pid64>>63)&1);
+	lcls1_timing = ts->mode || ((ts->pid64>>63)&1);
 
         pT->plock->lock();
         pT->time.secPastEpoch = dp[5];
         pT->time.nsec = dp[4];
         pT->rawpid = ts->pid64;
-        pT->pid64 = lcls1_mode ? pT->time.nsec & 0x1ffff: ts->pid64;
+        pT->pid64 = lcls1_timing ? pT->time.nsec & 0x1ffff: ts->pid64;
         pT->plock->unlock();
 
         if(!ts->mode) {    // LCLS2 mode timestamp update, using the best timestamp
@@ -256,7 +259,7 @@ static void tprChannelFunc(void *param)
         }
 
 	// ch11 and LCLS1 mode, handle the LCLS1 timestamp and low number events
-        if(master && lcls1_mode) {   
+        if(master && lcls1_timing) {   
             int timeslot = (dp[6] >> 16 & 0x7);
             if(timeslot == 1 || timeslot == 4) {    // update active timeslot timestamp for TSE=-1
                 ts_tbl_t *pT0 = &(ts_tbl[0]);
